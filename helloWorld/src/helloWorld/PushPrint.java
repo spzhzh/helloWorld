ccca0034d01fa2b0b5d671c10926edd921d8b686
spzhzh <jonah0711@naver.com>
2016-11-14 10:43:03 +0900

    Update PushPrint.java

--- helloWorld/src/helloWorld/PushPrint.java
+++ helloWorld/src/helloWorld/PushPrint.java
@@ -5,7 +5,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("add java Class File push");
-		Ststem.out.println("World with 365");
+		System.out.println("World with 365");
 	}
 
 }
